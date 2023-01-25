7d6cf03c65cbbb4004fbaf89385947f3ce60beee
vfries <vfries@student.42lyon.fr>
2023-01-25 16:38:11 +0100

    Added ft_hm_size() to libft

--- lib/libft/include/ft_hashmap.h
+++ lib/libft/include/ft_hashmap.h
@@ -6,7 +6,7 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 14:23:47 by vfries            #+#    #+#             */
-/*   Updated: 2023/01/17 15:52:24 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2023/01/25 16:34:20 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,6 @@ void				*ft_hm_get_content(t_hashmap map, char *target);
 t_hashmap_content	*ft_hm_get_elem(t_hashmap map, char *target);
 size_t				ft_hm_get_index(char *target);
 t_hashmap			ft_hm_init(void);
+int					ft_hm_size(t_hashmap env_variables);
 
 #endif
