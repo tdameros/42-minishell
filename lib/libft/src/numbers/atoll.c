65741faf9b8e8dbdc52b9df72c34b05bb4298a79
Vincent Fries <vfries@z2r12p4.42lyon.fr>
2023-02-12 20:57:39 +0100

    Fixed exit

--- lib/libft/src/numbers/atoll.c
+++ lib/libft/src/numbers/atoll.c
@@ -6,37 +6,55 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 12:19:01 by vfries            #+#    #+#             */
-/*   Updated: 2023/01/09 01:03:30 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2023/02/12 20:57:00 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_char.h"
 #include <limits.h>
+#include <stdbool.h>
+#include <sys/errno.h>
+
+static long long	handle_overflow(bool is_negative, long long tmp_number,
+						char next_char);
 
 long long	ft_atoll(const char *str)
 {
 	long long	result;
 	long long	tmp;
-	char		is_negative;
+	bool		is_negative;
 
-	while (*str == '\t' || *str == '\n' || *str == '\v' || *str == '\f'
-		|| *str == '\r' || *str == ' ')
+	while (ft_isspace(*str))
 		str++;
 	is_negative = *str == '-';
 	if (*str == '+' || *str == '-')
 		str++;
+	if (*str < '0' || *str > '9')
+		errno = EINVAL;
 	result = 0;
 	while (*str >= '0' && *str <= '9')
 	{
 		tmp = result * 10 + *str++ - '0';
 		if (tmp < result)
-		{
-			if (is_negative)
-				return (LLONG_MIN);
-			return (LLONG_MAX);
-		}
+			return (handle_overflow(is_negative, tmp, *str));
 		result = tmp;
 	}
+	if (*str != '\0')
+		errno = EINVAL;
 	if (is_negative)
 		return (-result);
 	return (result);
+}
+
+static long long	handle_overflow(bool is_negative, long long tmp_number,
+						char next_char)
+{
+	if (is_negative)
+	{
+		if (tmp_number != LLONG_MIN || ft_isdigit(next_char))
+			errno = ERANGE;
+		return (LLONG_MIN);
+	}
+	errno = ERANGE;
+	return (LLONG_MAX);
 }
--- src/built-in/exit.c
+++ src/built-in/exit.c
@@ -6,7 +6,7 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 00:18:00 by tomy              #+#    #+#             */
-/*   Updated: 2023/02/05 23:09:31 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2023/02/12 20:46:40 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,26 @@
 #include "exit_code.h"
 #include "built_in.h"
 
+#include <stdio.h>
 int	exit_builtin(char **args)
 {
-	unsigned char	status;
+	long long	status;
 
 	write(1, "exit\n", 5);
 	if (args[1] == NULL)
 		exit(0);
-	if (!ft_is_number(args[1]))
-	{
-		print_error("exit", args[1], "numeric argument required");
-		exit(2);
-	}
 	if (args[2] != NULL)
 	{
 		print_error("exit", NULL, "too many arguments");
 		return (exit_code(1));
 	}
-	status = ft_atoi(args[1]);
+	errno = 0;
+	status = ft_atoll(args[1]);
+	// printf("%lld\n", status);
+	if (errno == ERANGE || errno == EINVAL)
+	{
+		print_error("exit", args[1], "numeric argument required");
+		exit(2);
+	}
 	exit(status);
 }
