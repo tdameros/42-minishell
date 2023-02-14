02f1d11b218ef614f41eee8b5afdedcb6214f1db
Riescent <github.herd7@aleeas.com>
2023-02-15 00:13:40 +0100

    Fixed terminal function failing when minishell is not run from a terminal

--- src/terminal.c
+++ src/terminal.c
@@ -6,7 +6,7 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 04:04:09 by vfries            #+#    #+#             */
-/*   Updated: 2023/02/14 22:30:48 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2023/02/15 00:09:02 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@ int	terminal_restore(const struct termios *termios_save)
 {
 	const int	ret = tcsetattr(STDERR_FILENO, TCSANOW, termios_save);
 
+	if (isatty(STDERR_FILENO) == 0 || isatty(STDOUT_FILENO) == 0
+		|| isatty(STDIN_FILENO) == 0)
+		return (0);
 	if (ret < 0)
 		print_error(NULL, "Failed to save termios\n", get_error());
 	return (ret);
@@ -27,6 +30,9 @@ int	terminal_save(struct termios *termios_save)
 {
 	const int	ret = tcgetattr(STDERR_FILENO, termios_save);
 
+	if (isatty(STDERR_FILENO) == 0 || isatty(STDOUT_FILENO) == 0
+		|| isatty(STDIN_FILENO) == 0)
+		return (0);
 	if (ret < 0)
 		print_error(NULL, "Failed to restore termios\n", get_error());
 	return (ret);
@@ -36,6 +42,9 @@ int	terminal_disable_ctrl_backslash_output(void)
 {
 	struct termios	term;
 
+	if (isatty(STDERR_FILENO) == 0 || isatty(STDOUT_FILENO) == 0
+		|| isatty(STDIN_FILENO) == 0)
+		return (0);
 	if (tcgetattr(STDIN_FILENO, &term) == -1)
 	{
 		print_error(NULL, "Failed to disable SIGQUIT\n", get_error());
