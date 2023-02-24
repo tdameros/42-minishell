1a47243177a0d571d66ecd95a84f7a2626db67d2
Tom Damerose <tdameros@z2r1p1.42lyon.fr>
2023-02-24 12:08:42 +0100

    fix CTRL + D and CTRL + C in interactive shell

--- src/parser/syntax.c
+++ src/parser/syntax.c
@@ -52,6 +52,10 @@ static bool	is_valid_operator_syntax(t_token *token, t_token *previous_token)
 	if (previous_token != NULL && previous_token->operator == HERE_DOC
 		&& token->type == OPERATOR)
 		return (false);
+	if (previous_token != NULL && token->operator == OPEN_PARENTHESES
+		&& (!is_separator_token(previous_token)
+			&& previous_token->operator != OPEN_PARENTHESES))
+		return (false);
 	if (token->operator == CLOSE_PARENTHESES && previous_token != NULL
 		&& previous_token->type == OPERATOR
 		&& previous_token->operator != CLOSE_PARENTHESES)
