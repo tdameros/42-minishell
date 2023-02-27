1da30f2f36111cd8c66516d8501911539c223ed8
Riescent <github.herd7@aleeas.com>
2023-02-27 20:38:55 +0300

    Added bitmask to libft

--- lib/libft/src/bitmask/bitmask_init.c
+++ lib/libft/src/bitmask/bitmask_init.c
@@ -1,27 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   bitmask_init.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/20 15:29:14 by vfries            #+#    #+#             */
-/*   Updated: 2023/02/25 19:33:54 by vfries           ###   ########lyon.fr   */
+/*   Created: 2023/02/25 12:41:59 by vfries            #+#    #+#             */
+/*   Updated: 2023/02/26 16:50:48 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_io.h"
-#include "ft_string_test.h"
-#include "ft_bitmask_test.h"
-#include "hashmap_test.h"
+#include "ft_bitmask.h"
+#include "ft_mem.h"
 
-int	main(void)
+int	ft_bitmask_init(t_bitmask *bitmask, size_t size)
 {
-	if (ft_join_strs_test() == false)
-		ft_putstr("ft_join_strs: KO\n");
-	if (ft_hashmap_test() == false)
-		ft_putstr("hashmap: KO\n");
-	if (ft_bitmask_test() == false)
-		ft_putstr("bitmask: KO\n");
-	ft_putstr("Tests done\n");
+	if (size % 8 == 0)
+		bitmask->arr = ft_calloc(size / 8, sizeof(*bitmask->arr));
+	else
+		bitmask->arr = ft_calloc(size / 8 + 1, sizeof(*bitmask->arr));
+	return (-(bitmask->arr == NULL));
 }
--- lib/libft/test/header/ft_bitmask_test.h
+++ lib/libft/test/header/ft_bitmask_test.h
@@ -1,27 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_bitmask_test.h                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/20 15:29:14 by vfries            #+#    #+#             */
-/*   Updated: 2023/02/25 19:33:54 by vfries           ###   ########lyon.fr   */
+/*   Created: 2023/02/25 19:17:05 by vfries            #+#    #+#             */
+/*   Updated: 2023/02/25 19:17:39 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_io.h"
-#include "ft_string_test.h"
-#include "ft_bitmask_test.h"
-#include "hashmap_test.h"
+#ifndef FT_BITMASK_TEST_H
+# define FT_BITMASK_TEST_H
 
-int	main(void)
-{
-	if (ft_join_strs_test() == false)
-		ft_putstr("ft_join_strs: KO\n");
-	if (ft_hashmap_test() == false)
-		ft_putstr("hashmap: KO\n");
-	if (ft_bitmask_test() == false)
-		ft_putstr("bitmask: KO\n");
-	ft_putstr("Tests done\n");
-}
+# include <stdbool.h>
+
+bool	ft_bitmask_test(void);
+
+#endif
